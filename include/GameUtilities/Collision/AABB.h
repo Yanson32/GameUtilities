27ffd9972b0b87d71ad5eb71e3473cf11abfb34c
Wayne Larson Jr <wlarson30@gmail.com>
2018-04-21 01:21:27 -0400

    Added AABB

---
+++ include/GameUtilities/Collision/AABB.h
@@ -0,0 +1,14 @@
+#ifndef GU_COLLISION_AABB_H
+#define GU_COLLISION_AABB_H
+
+
+class AABB
+{
+    public:
+        AABB();
+        virtual ~AABB();
+    protected:
+    private:
+};
+
+#endif // GU_COLLISION_AABB_H
---
+++ src/GameUtilities/Collision/AABB.cpp
@@ -0,0 +1,11 @@
+#include "GameUtilities/Collision/AABB.h"
+
+AABB::AABB()
+{
+    //ctor
+}
+
+AABB::~AABB()
+{
+    //dtor
+}
