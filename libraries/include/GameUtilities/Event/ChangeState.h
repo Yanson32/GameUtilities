26765c9897ddd57bdb9c6a53899f7ca8f91a093c
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-20 22:52:48 -0300

    Updated the comments of the ChangeState class

--- libraries/include/GameUtilities/Event/ChangeState.h
+++ libraries/include/GameUtilities/Event/ChangeState.h
@@ -1,3 +1,10 @@
+/*******************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   5/20/19
+*   @brief  The changestate class is a subclass of the
+*           Event class and is used to signal a change
+*           in the games state.
+***********************************************************/
 #ifndef GAMEUTILITIES_CHANGESTATE_H
 #define GAMEUTILITIES_CHANGESTATE_H
 #include "event_export.h"
@@ -7,12 +14,24 @@ namespace GU
 {
     namespace Evt
     {
-
 		class EVENT_EXPORT ChangeState: public Event
 		{
 			public:
+                /*******************************************************//**
+                *   @brief  Constructor
+                *   @param  newStateId a unique identifier of the new state
+                *   @param  newLine the line where the event originates from
+                *   @param  newFile the file where the event originates from
+                ***********************************************************/
 				ChangeState(const int newStateId, const int newLine = 0, const char* newFile = "");
-				const int stateId;
+
+
+				const int stateId;  //The unique identifier of the state
+
+
+                /*******************************************************//**
+                *   @brief  Destructor
+                ***********************************************************/
 				virtual ~ChangeState();
 		};
 	}
--- libraries/src/GameUtilities/Event/ChangeState.cpp
+++ libraries/src/GameUtilities/Event/ChangeState.cpp
@@ -4,6 +4,13 @@ namespace GU
 {
     namespace Evt
     {
+
+        /*******************************************************//**
+        *   @brief  Constructor
+        *   @param  newStateId a unique identifier of the new state
+        *   @param  newLine the line where the event originates from
+        *   @param  newFile the file where the event originates from
+        ***********************************************************/
 		ChangeState::ChangeState(const int newStateId, const int newLine, const char* newFile):
 		Event(GU::Evt::Id::CHANGE_STATE, newLine, newFile),
 		stateId(newStateId)
@@ -11,6 +18,10 @@ namespace GU
 			//ctor
 		}
 
+
+        /*******************************************************//**
+        *   @brief  Destructor
+        ***********************************************************/
 		ChangeState::~ChangeState()
 		{
 			//dtor
