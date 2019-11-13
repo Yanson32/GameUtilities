29d195d07da5e0298fb39f8621361b170e87d8b3
Yanson <wlarson30@gmail.com>
2019-11-13 08:37:48 -0500

    Added a click event

---
+++ libraries/include/GameUtilities/Event/Click.h
@@ -0,0 +1,16 @@
+#ifndef CLICK_H
+#define CLICK_H
+
+
+class Click
+{
+    public:
+        Click(const int newId);
+        virtual ~Click();
+        const int id;
+    protected:
+
+    private:
+};
+
+#endif // CLICK_H
---
+++ libraries/src/GameUtilities/Event/Click.cpp
@@ -0,0 +1,12 @@
+#include "GameUtilities/Event/Click.h"
+
+Click::Click(const int newId):
+id(newId)
+{
+    //ctor
+}
+
+Click::~Click()
+{
+    //dtor
+}
