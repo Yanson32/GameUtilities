a03fee1a88262cf37bc28b2b0f7eb306c000382f
Yanson <wlarson30@gmail.com>
2019-11-24 17:34:49 -0500

    Refactor: Removed the SFML directory

--- libraries/src/GameUtilities/SFML/ResourceManager.cpp
+++
@@ -1,6 +0,0 @@
-
-#include "ResourceManager.h"
-
-ResourceHolder<sf::Font, sf::String> ResourceManager::font;
-ResourceHolder<sf::Texture, sf::String> ResourceManager::texture;
-//ResourceHolder<sf::SoundBuffer, sf::String> ResourceManager::buffer;
