bdf5af15767125218c9a038492b5d9a68b7b7114
Yanson <wlarson30@gmail.com>
2019-12-02 02:10:15 -0500

    Updated code for windows compatibility

--- libraries/include/GameUtilities/Event/Click.h
+++ libraries/include/GameUtilities/Event/Click.h
@@ -1,11 +1,12 @@
 #ifndef CLICK_H
 #define CLICK_H
+#include "event_export.h"
 #include "GameUtilities/Event/Event.h"
 namespace GU
 {
     namespace Evt
     {
-        class Click: public Event
+        class EVENT_EXPORT Click: public Event
         {
             public:
                 Click(const int newId);
