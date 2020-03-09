1ef60c9c2ea6f6822d2360f3ac05a73cbf65a2ab
Yanson <wlarson30@gmail.com>
2020-03-08 23:14:57 -0400

    Refactor: Updated the tests for GU::Ps::Container

--- test/GameUtilities/ParticleSystem/ParticleSytemContainerTest.cpp
+++ test/GameUtilities/ParticleSystem/ParticleSytemContainerTest.cpp
@@ -17,10 +17,16 @@ TEST_CASE("ParticleSystem::Container Bracket operator")
 
 TEST_CASE("ParticleSystem::Container emplace_back method")
 {
+    //Setup code
     GU::PS::Container<int> cont(10);
-    cont.emplace_back<int>(6);
+    REQUIRE(cont.size() == 0);
 
-    REQUIRE(cont.size() == 1);
+    SECTION("Testing emplace back")
+    {
+        cont.emplace_back<int>(6);
+
+        REQUIRE(cont.size() == 1);
+    }
 }
 
 TEST_CASE("ParticleSystem::Container size method")
@@ -36,22 +42,46 @@ TEST_CASE("ParticleSystem::Container remove method")
     cont.emplace_back<int>(0);
     cont.emplace_back<int>(1);
     cont.emplace_back<int>(2);
-    cont.remove(1);
-    REQUIRE(cont.size() == 2);
-    REQUIRE(cont.capacity() == 10);
-    REQUIRE(cont[0] == 0);
-    REQUIRE(cont[1] == 2);
 
+    SECTION("Test removing a single element ")
+    {
+        cont.remove(1);
+        REQUIRE(cont.size() == 2);
+        REQUIRE(cont.capacity() == 10);
+        REQUIRE(cont[0] == 0);
+        REQUIRE(cont[1] == 2);
+    }
+    
 }
 
 TEST_CASE("ParticleSystem::Container capacity method")
 {
     GU::PS::Container<int> cont(10);
-    REQUIRE(cont.capacity() == 10);
+
+    SECTION("Testing default capacity")
+    {
+        REQUIRE(cont.capacity() == 10);
+    }
+
+    SECTION("Testing capacity change after adding an element")
+    {
+        cont.emplace_back(2);
+        REQUIRE(cont.capacity() == 10);
+    }
 }
 
 TEST_CASE("ParticleSystem::Container empty method")
 {
     GU::PS::Container<int> cont(10);
-    REQUIRE(cont.empty());
+
+    SECTION("Container should be empty")
+    {
+        REQUIRE(cont.empty());
+    }
+
+    SECTION("Container should not be empty")
+    {
+        cont.emplace_back(2);
+        REQUIRE(!cont.empty());
+    }
 }
