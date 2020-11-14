77c8eadfc2e0640308f2c96583bff02f42e3569e
Yanson32 <wlarson30@gmail.com>
2020-11-14 02:24:42 -0500

    Added message event

---
+++ libraries/src/GameUtilities/Event/MessageEvent.cpp
@@ -0,0 +1,24 @@
+#include "GameUtilities/Event/MessageEvent.h"
+
+namespace GU
+{
+    namespace Evt
+    {
+        MessageEvent::MessageEvent(const std::string &msg, const int messageType):
+        m_messageType(messageType)
+        {
+            //ctor
+        }
+
+        int MessageEvent::getMessageType() const
+        {
+            return m_messageType;
+        }
+        MessageEvent::~MessageEvent()
+        {
+            //dtor
+        }
+    }
+}
+
+
