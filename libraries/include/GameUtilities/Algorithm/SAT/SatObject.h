d2628b24e288fad063542b3083eff6cac7f1fd39
Yanson <wlarson30@gmail.com>
2022-04-01 10:05:14 -0400

    Added Algorithm directory

---
+++ libraries/include/GameUtilities/Algorithm/SAT/SatObject.h
@@ -0,0 +1,12 @@
+#ifndef SAT_OBJECT_H
+#define SAT_OBJECT_H
+#include "algorithm_export.h"
+
+class ALGORITHM_EXPORT SatObject
+{
+  public:
+    SatObject();
+    virtual ~SatObject();
+};
+
+#endif
