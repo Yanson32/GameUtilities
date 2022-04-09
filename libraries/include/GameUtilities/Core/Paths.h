29ba990f279c1702f79e5f4921b61284c79b85f7
Yanson <wlarson30@gmail.com>
2022-04-08 21:30:25 -0400

    Added the Paths file to define functions to get commonly used paths on different operating systems

---
+++ libraries/include/GameUtilities/Core/Paths.h
@@ -0,0 +1,9 @@
+#ifndef GAMEUTILITIES_PATH_H
+#define	GAMEUTILITIES_PATH_H
+
+#include "core_export.h"
+#include "GameUtilities/Core/String.h"
+
+GU::Core::String CORE_EXPORT getConfigDir(const GU::Core::String &appName = "");
+
+#endif
