0bcc61715cd5434b425f6b8b811fcda7143452d2
Yanson <wlarson30@gmail.com>
2022-04-09 00:11:07 -0400

    Added namespace to Paths file

--- libraries/src/GameUtilities/Core/Paths.cpp
+++ libraries/src/GameUtilities/Core/Paths.cpp
@@ -7,30 +7,34 @@
 #include <cstdlib>
 #endif
 
-
-/**************************************************************************
-* @brief:    This function returns a os specific path to the appropriate 1
-*            directory for a config file.
-* @param     appName is the name of the application. The name will be 
-*            added to the end of the path. The final path may not exist 
-*            and so it may need to be created by the caller.
-**************************************************************************/
-GU::Core::String getConfigDir(const GU::Core::String &appName)
+namespace GU 
 {
-    #ifdef _WIN32
-    return SHGetKnownFolderPath(CSIDL_APPDATA) + "/" + appName.toStdString();
-    #endif 
+    namespace Core 
+    {
+        /**************************************************************************
+        * @brief:    This function returns a os specific path to the appropriate 1
+        *            directory for a config file.
+        * @param     appName is the name of the application. The name will be 
+        *            added to the end of the path. The final path may not exist 
+        *            and so it may need to be created by the caller.
+        **************************************************************************/
+        GU::Core::String getConfigDir(const GU::Core::String &appName)
+        {
+            #ifdef _WIN32
+            return SHGetKnownFolderPath(CSIDL_APPDATA) + "/" + appName.toStdString();
+            #endif 
     
-    #ifdef linux
-    GU::Core::String temp;
-    temp = getenv("HOME");
-    temp += "/";
-    temp += ".config"; 
-    temp += "/";
-    temp += appName;
-    return temp; 
-    #endif	
+            #ifdef linux
+            GU::Core::String temp;
+            temp = getenv("HOME");
+            temp += "/";
+            temp += ".config"; 
+            temp += "/";
+            temp += appName;
+            return temp; 
+            #endif	
     
-    return ""; 
+            return ""; 
+        }
+    }
 }
-
