17f35b44f483c663f2a0408185b9bb256ad9d058
Yanson <wlarson30@gmail.com>
2022-05-02 02:57:19 -0400

    Updated the FrameRate class to include comments. I also updated the names of data members

--- libraries/include/GameUtilities/Core/FrameRate.h
+++ libraries/include/GameUtilities/Core/FrameRate.h
@@ -1,5 +1,10 @@
 #ifndef GAMEUTILITIES_FRAMERATE_H
 #define GAMEUTILITIES_FRAMERATE_H
+/*********************************************************************************//**
+*	@author	Wayne J Larson Jr.
+*   @date 12/10/2017
+*   @description This class allows us to track the frame rate of a game.
+*************************************************************************************/
 #include "core_export.h"
 #include <cstddef> //std::size_t
 
@@ -12,14 +17,51 @@ namespace GU
         class CORE_EXPORT FrameRate final
         {
             public:
-                FrameRate(const std::size_t newMaxQueueSize = 60);
+                /******************************************************************//**
+                *   @brief  Constructor
+                *   @param  maxQueueSize the maximum size of the queue. The queue stores
+                *           a time for each frame, which is then used to get the average
+                *           time. 
+                **********************************************************************/ 
+                FrameRate(const std::size_t maxQueueSize = 60);
+                
+
+                /******************************************************************//**
+                *   @brief  This method must be called once per frame. It adds the 
+                *           execution time of the previous frame to the queue, and 
+                *           also removes an entry if the number of entries is over 
+                *           maxQueueSize.
+                **********************************************************************/ 
                 void update();
+                
+
+                /******************************************************************//**
+                *   @brief  This method returns the average frame rate.
+                *   @param  The average frame rate in seconds 
+                **********************************************************************/ 
                 double getFrameRate();
+                
+
+                /******************************************************************//**
+                *   @brief  Destructor 
+                **********************************************************************/ 
                 ~FrameRate();
             private:
+                
+                /******************************************************************//**
+                *   @brief  Deleted copy constructor 
+                *   @param  The FrameRate class to be copied  
+                **********************************************************************/ 
                 FrameRate(const FrameRate&) = delete;
+                
+
+                /******************************************************************//**
+                *   @brief  Deleted assignment operator 
+                *   @param  The FrameRate class to be assigned 
+                **********************************************************************/ 
                 FrameRate& operator = (const FrameRate&) = delete;
-				class Impl;
+			
+            	class Impl;
 				Impl* pimpl = nullptr;
         };
     }
--- libraries/src/GameUtilities/Core/FrameRate.cpp
+++ libraries/src/GameUtilities/Core/FrameRate.cpp
@@ -1,9 +1,3 @@
-/*!
-*	\author Wayne J Larson Jr.
-*	\version 1.0
-*	\date 12/10/17
-*/
-
 #include "GameUtilities/Core/FrameRate.h"
 #include <iomanip>
 #include <ctime>
@@ -16,76 +10,144 @@ namespace GU
 		class FrameRate::Impl
 		{
 			public:
-				Impl(const std::size_t newMaxQueueSize = 0);
+                /******************************************************************//**
+                *   @brief  Constructor
+                *   @param  maxQueueSize the maximum size of the queue. The queue stores
+                *           a time for each frame, which is then used to get the average
+                *           time. 
+                **********************************************************************/ 
+				Impl(const std::size_t maxQueueSize = 0);
+                
+
+                /******************************************************************//**
+                *   @brief  This method must be called once per frame. It adds the 
+                *           execution time of the previous frame to the queue, and 
+                *           also removes an entry if the number of entries is over 
+                *           maxQueueSize.
+                **********************************************************************/ 
 				void update();
+                
+
+                /******************************************************************//**
+                *   @brief  This method returns the average frame rate.
+                *   @param  The average frame rate in seconds 
+                **********************************************************************/ 
 				double getFrameRate();
+                
+
+                /******************************************************************//**
+                *   @brief  Destructor 
+                **********************************************************************/ 
 				~Impl();
 			private:
-				const std::size_t maxQueueSize;
-				std::deque<unsigned int> timeQue;
-				unsigned int prevTime = 0;
+				const std::size_t m_maxQueueSize;
+				std::deque<unsigned int> m_timeQue;
+				unsigned int m_prevTime = 0;
 		};
 
-		FrameRate::Impl::Impl(const std::size_t newMaxQueueSize): maxQueueSize(newMaxQueueSize)
+        /******************************************************************//**
+        *   @brief  Constructor
+        *   @param  maxQueueSize the maximum size of the queue. The queue stores
+        *           a time for each frame, which is then used to get the average
+        *           time. 
+        **********************************************************************/ 
+		FrameRate::Impl::Impl(const std::size_t maxQueueSize): m_maxQueueSize(maxQueueSize)
 		{
-			prevTime = clock();
+			m_prevTime = clock();
 		}
+        
+
+        /******************************************************************//**
+        *   @brief  This method must be called once per frame. It adds the 
+        *           execution time of the previous frame to the queue, and 
+        *           also removes an entry if the number of entries is over 
+        *           maxQueueSize.
+        **********************************************************************/ 
 		void FrameRate::Impl::update()
 		{
 			//get the current time in milliseconds
 			unsigned int currentTime = clock();
 
 			//add the current time to the dequeue
-			timeQue.push_front(currentTime - prevTime);
+			m_timeQue.push_front(currentTime - m_prevTime);
 
 			//remove any time that is over maxQueueSize
-			if (timeQue.size() >= maxQueueSize)
-				timeQue.pop_back();
+			if (m_timeQue.size() >= m_maxQueueSize)
+				m_timeQue.pop_back();
 
 			//set previous time to current time
-			prevTime = currentTime;
+			m_prevTime = currentTime;
 		}
 
+        
+        /******************************************************************//**
+        *   @brief  This method returns the average frame rate.
+        *   @param  The average frame rate in seconds 
+        **********************************************************************/ 
 		double FrameRate::Impl::getFrameRate()
 		{
 			//add all the time in the deque
 			unsigned int totalTime = 0;
-			for (auto element : timeQue)
+			for (auto element : m_timeQue)
 				totalTime += element;
 
 			//calculate the average time
-			double averageTime = static_cast<double>(totalTime) / timeQue.size();
+			double averageTime = static_cast<double>(totalTime) / m_timeQue.size();
 
 			//averageTime needs to be converted to milliseconds
 			//then we divide 1 by the result witch gives the number of frames per second
 			return 1 / (averageTime / 1000);
 		}
 
+        
+        /******************************************************************//**
+        *   @brief  Destructor 
+        **********************************************************************/ 
 		FrameRate::Impl::~Impl()
 		{
 
 		}
-		FrameRate::FrameRate(const std::size_t newMaxQueueSize):
-		pimpl(new FrameRate::Impl(newMaxQueueSize))
+        
+
+        /******************************************************************//**
+        *   @brief  Constructor
+        *   @param  maxQueueSize the maximum size of the queue. The queue stores
+        *           a time for each frame, which is then used to get the average
+        *           time. 
+        **********************************************************************/ 
+		FrameRate::FrameRate(const std::size_t maxQueueSize):
+		pimpl(new FrameRate::Impl(maxQueueSize))
 		{
 			//ctor
 
 		}
 
 
+        /******************************************************************//**
+        *   @brief  This method must be called once per frame. It adds the 
+        *           execution time of the previous frame to the queue, and 
+        *           also removes an entry if the number of entries is over 
+        *           maxQueueSize.
+        **********************************************************************/ 
 		void FrameRate::update()
 		{
 			pimpl->update();
 		}
 
 
-		//calculate the number of frames per seconds
+        /******************************************************************//**
+        *   @brief  This method returns the average frame rate.
+        *   @param  The average frame rate in seconds 
+        **********************************************************************/ 
 		double FrameRate::getFrameRate()
 		{
 			return pimpl->getFrameRate();
 		}
 
 
+        /******************************************************************//**
+        *   @brief  Destructor 
+        **********************************************************************/ 
 		FrameRate::~FrameRate()
 		{
 			//dtor
