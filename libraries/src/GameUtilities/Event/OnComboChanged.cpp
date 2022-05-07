d199e34339d00c3bf6ee737f18c26625d22f408d
Yanson <wlarson30@gmail.com>
2022-05-07 17:54:40 -0400

    Added comments to the OnComboChanged evetn

--- libraries/src/GameUtilities/Event/OnComboChanged.cpp
+++ libraries/src/GameUtilities/Event/OnComboChanged.cpp
@@ -4,6 +4,11 @@ namespace GU
 {
     namespace Evt
     {
+        /**************************************************************************
+        *   @brief  Constructor   
+        *   @param  newComboId is the id of the combo box
+        *   @param  newIndex is the index of the data element that has changed. 
+        **************************************************************************/
         OnComboChanged::OnComboChanged(const int newComboId, const int newIndex):
         Event(static_cast<int>(GU::Evt::EventId::ON_COMBO_CHANGED)),
         comboId(newComboId),
@@ -12,6 +17,11 @@ namespace GU
             //ctor
         }
 
+        
+
+        /**************************************************************************
+        *   @brief  Destructor
+        **************************************************************************/
         OnComboChanged::~OnComboChanged()
         {
             //dtor
