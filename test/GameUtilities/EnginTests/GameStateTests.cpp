04d20e9c88a41c37cbe2e1e818a3d2297c624714
Yanson <wlarson30@gmail.com>
2022-05-08 16:07:25 -0400

    Added the Catch2 library

--- test/GameUtilities/EnginTests/GameStateTests.cpp
+++ test/GameUtilities/EnginTests/GameStateTests.cpp
@@ -1,5 +1,4 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include <catch2/catch_test_macros.hpp>
 #include <GameUtilities/Engin/GameState.h>
 
 class TestState: public GU::Engin::GameState
