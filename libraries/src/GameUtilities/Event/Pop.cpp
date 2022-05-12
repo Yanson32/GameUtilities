6b83fed54c03f2ffbf754170996bfe38931ae410
Yanson <wlarson30@gmail.com>
2022-05-12 08:31:16 -0400

    Updated the Pop event. Comments and style

--- libraries/src/GameUtilities/Event/Pop.cpp
+++ libraries/src/GameUtilities/Event/Pop.cpp
@@ -5,12 +5,23 @@ namespace GU
 {
     namespace Evt
     {
-		Pop::Pop(const int newLine, const char* newFile):
-		Event(static_cast<int>(GU::Evt::EventId::POP_STATE), newLine, newFile)
+        /**************************************************************************
+        *   @brief  Constructor
+        *   @param  id is the unique identifier for the pop event.
+        *   @param  line is the line number where the event originated from
+        *   @param  file is the file where the event originated from
+        **************************************************************************/
+		Pop::Pop(const int id, const int line, const char* file):
+		Event(static_cast<int>(GU::Evt::EventId::POP_STATE), line, file),
+        m_id(id)
 		{
 			//ctor
 		}
 
+        
+        /**************************************************************************
+        *   @brief  Deconstructor 
+        **************************************************************************/
 		Pop::~Pop()
 		{
 			//dtor
