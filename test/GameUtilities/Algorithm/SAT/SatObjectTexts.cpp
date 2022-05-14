33760887742831c9508779718b4f7ce66d411937
Yanson <wlarson30@gmail.com>
2022-05-14 01:37:52 -0400

    Added Tests for SatObject.cpp

---
+++ test/GameUtilities/Algorithm/SAT/SatObjectTexts.cpp
@@ -0,0 +1,13 @@
+#include <catch2/catch_test_macros.hpp>
+#include "GameUtilities/Algorithm/SatObject.hpp"
+
+
+TEST_CASE( "lla;skdj" ) 
+{
+    GU::Al::SatObject sat;
+}
+
+TEST_CASE( "ddddd" ) 
+{
+
+}
