e6bcfbed20bb39d1cc5d138aaa1196da5f40b394
Yanson <wlarson30@gmail.com>
2022-05-14 09:20:13 -0400

    Added tests for SatObject

--- test/GameUtilities/Algorithm/SAT/SatObjectTexts.cpp
+++ test/GameUtilities/Algorithm/SAT/SatObjectTexts.cpp
@@ -1,10 +1,12 @@
 #include <catch2/catch_test_macros.hpp>
-#include "GameUtilities/Algorithm/SatObject.hpp"
+#include "GameUtilities/Algorithm/SAT/SatObject.h"
 
 
-TEST_CASE( "lla;skdj" ) 
+TEST_CASE( "SatObject::Constructor" ) 
 {
-    GU::Al::SatObject sat;
+    GU::Al::SatObject sat(Math::Vector2<float>(100, 100), 3 );
+    REQUIRE(sat.getVertexCount() == 3);
+    REQUIRE(sat.getEdgeCount() == 3);
 }
 
 TEST_CASE( "ddddd" ) 
