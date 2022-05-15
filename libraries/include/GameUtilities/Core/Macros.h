c362d9f6d957822c0742808b767c777fc8a00f95
Yanson <wlarson30@gmail.com>
2022-05-15 14:03:32 -0400

    Added examples for macros

--- libraries/include/GameUtilities/Core/Macros.h
+++ libraries/include/GameUtilities/Core/Macros.h
@@ -5,7 +5,7 @@
 
 #define STR(x) #x
 
-#define GU_ASSERT(x) if (!(x)) { printf("GU assert failed: (%s), function %s, file %s, line %d.\n", STR(x), __PRETTY_FUNCTION__, __FILE__, __LINE__); throw std::runtime_error(x); }
+#define GU_ASSERT(x) if (!(x)) { printf("GU assert failed: (%s), function %s, file %s, line %d.\n", STR(x), __PRETTY_FUNCTION__, __FILE__, __LINE__); throw std::runtime_error(STR(x)); }
  
 
 #endif
