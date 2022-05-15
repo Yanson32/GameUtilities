082b91eb7c59523ddab3556180f0548b8c35436c
Yanson <wlarson30@gmail.com>
2022-05-15 13:41:08 -0400

    Added assertion macro

--- libraries/include/GameUtilities/Core/Macros.h
+++ libraries/include/GameUtilities/Core/Macros.h
@@ -3,4 +3,9 @@
 
 #define UNUSED(x) (void)(x)
 
+#define STR(x) #x
+
+#define GU_ASSERT(x) if (!(x)) { printf("GU assert failed: (%s), function %s, file %s, line %d.\n", STR(x), __PRETTY_FUNCTION__, __FILE__, __LINE__); throw std::runtime_error(x); }
+ 
+
 #endif
