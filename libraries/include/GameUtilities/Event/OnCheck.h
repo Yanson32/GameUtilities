690a06036539774738bc707cbad5a87036b38af4
Yanson <wlarson30@gmail.com>
2022-05-16 11:28:16 -0400

    Updated the comments for the wOnCheck event

--- libraries/include/GameUtilities/Event/OnCheck.h
+++ libraries/include/GameUtilities/Event/OnCheck.h
@@ -1,5 +1,10 @@
 #ifndef GAMEUTILITIES_ON_CHECK_H 
 #define GAMEUTILITIES_ON_CHECK_H 
+/**************************************************************************
+*   Author:     Wayne J Larson Jr.
+*   Date:       5/16/22
+*   Purpose:    This event is generated when the user clicks on something. 
+**************************************************************************/
 #include "event_export.h"
 #include "GameUtilities/Event/Event.h"
 
@@ -10,12 +15,21 @@ namespace GU
         class EVENT_EXPORT OnCheck: public Event
         {
             public:
-                OnCheck(const int newCheckBoxId, const bool newChecked);
-                const int checkboxId = -1;
-                const bool checked;
+                /**************************************************************************
+                *   @breif  Constructor
+                *   @param  checkBoxId is a unique identifier for the checkbox.
+                *   @param  checked is true if the checkbox was checked and false otherwise.
+                **************************************************************************/
+                OnCheck(const int checkBoxId, const bool checked);
+                
+
+                /**************************************************************************
+                *   @breif  Destructor 
+                **************************************************************************/
                 virtual ~OnCheck();
-            protected:
-            private:
+            public: 
+                const int m_checkboxId = -1;
+                const bool m_checked;
 
         };
     }
