b214535a4dc443ae1d897be91285c56f994eddd9
Yanson <wlarson30@gmail.com>
2022-05-19 21:04:48 -0400

    Updated the LogFormatter class with comments

--- libraries/src/GameUtilities/Log/LogFormatter.cpp
+++ libraries/src/GameUtilities/Log/LogFormatter.cpp
@@ -9,42 +9,151 @@ namespace GU
 {
     namespace Log
     {
-      class LogFormatter::Impl
-      {
-        public:
-          Impl(){};
-          virtual ~Impl(){};
-          std::vector<std::shared_ptr<LogComponent>> m_data;
-          #ifdef MULTITHREAD
-            mutable std::mutex eventQueueLock;
-          #endif
-      };
+        class LogFormatter::Impl
+        {
+            public:
+                /*********************************************************************************//**
+                *	@brief  Constructor 
+                *************************************************************************************/
+                Impl();
+                
+    
+                /*********************************************************************************//**
+                *	@brief  Destructor 
+                *************************************************************************************/
+                virtual ~Impl();
+            public:
+                std::vector<std::shared_ptr<LogComponent>> m_data;
+        };
 
-  		LogFormatter::LogFormatter(): m_pimpl(new LogFormatter::Impl())
-  		{
+    
+        /*********************************************************************************//**
+        *	@brief  Constructor 
+        *************************************************************************************/
+        LogFormatter::Impl::Impl()
+        {
+
+        }
+        
+
+        /*********************************************************************************//**
+        *	@brief  Destructor 
+        *************************************************************************************/
+        LogFormatter::Impl::~Impl()
+        {
+
+        }
+        
+
+
+        /*********************************************************************************//**
+        *	@brief  Constructor 
+        *************************************************************************************/
+        LogFormatter::LogFormatter(): m_pimpl(new LogFormatter::Impl())
+        {
             assert(m_pimpl != nullptr);
-  		}
+        }
 
-      void LogFormatter::add(std::shared_ptr<GU::Log::LogComponent> component)
-      {
-        assert(m_pimpl != nullptr);
-        //assert(std::find(pimpl->m_data.begin(), pimpl->m_data.end(), component) != pimpl->m_data.end());
-        m_pimpl->m_data.push_back(component);
-      }
-
-      std::shared_ptr<LogComponent> LogFormatter::getComponent(const componentId &component) const
-      {
-        assert(m_pimpl != nullptr);
-        for(auto &element : m_pimpl->m_data)
-          if(element->m_id == component)
-            return element;
-
-        return nullptr;
-      }
+        
+        /*********************************************************************************//**
+        *	@brief  This method adds a log component to the log formatter. The log component
+        *           is used by the formatter to determine the order that the components will 
+        *           be written to the log file. Components will be written to the log file
+        *           in the same order they were added to the formatter.
+        *   @param  component is the log component to be added to the log file. 
+        *************************************************************************************/
+        void LogFormatter::add(std::shared_ptr<GU::Log::LogComponent> component)
+        {
+            assert(m_pimpl != nullptr);
+            //assert(std::find(pimpl->m_data.begin(), pimpl->m_data.end(), component) != pimpl->m_data.end());
+            m_pimpl->m_data.push_back(component);
+        }
 
 
-      GU::Core::String LogFormatter::format()
-      {
+        /*********************************************************************************//**
+        *	@brief  This method is used to initialize a log component. The log component should
+        *           have been added previously with the add method. 
+        *   @pre    A log component should be added with the add method.
+        *   @param  component will be used to initialize the previously added log component. 
+        *************************************************************************************/
+        void LogFormatter::init(std::shared_ptr<GU::Log::LogComponent> component)
+        {
+            assert(m_pimpl != nullptr);
+            for(std::size_t i = 0; i < m_pimpl->m_data.size(); ++i)
+            {
+                if((*m_pimpl->m_data[i]) == (*component))
+                {
+                    switch((*m_pimpl->m_data[i]).m_id)
+                    {
+                        case GU::Log::componentId::DATE:
+                        {
+                            std::shared_ptr<GU::Log::DateComponent> temp = std::dynamic_pointer_cast<DateComponent>(m_pimpl->m_data[i]);
+                            std::shared_ptr<GU::Log::DateComponent> temp2 = std::dynamic_pointer_cast<DateComponent>(component);
+                            (*temp) = (*temp2);
+                        }
+                        break;
+                        case GU::Log::componentId::FILE:
+                        {
+                            std::shared_ptr<GU::Log::FileComponent> temp = std::dynamic_pointer_cast<FileComponent>(m_pimpl->m_data[i]);
+                            std::shared_ptr<GU::Log::FileComponent> temp2 = std::dynamic_pointer_cast<FileComponent>(component);
+                            (*temp) = (*temp2);
+                        }
+                        break;
+                        case GU::Log::componentId::MSG:
+                        {
+                          std::shared_ptr<GU::Log::MsgComponent> temp = std::dynamic_pointer_cast<MsgComponent>(m_pimpl->m_data[i]);
+                          std::shared_ptr<GU::Log::MsgComponent> temp2 = std::dynamic_pointer_cast<MsgComponent>(component);
+                          (*temp) = (*temp2);
+                        }
+                        break;
+                        case GU::Log::componentId::SEVERITY:
+                        {
+                            std::shared_ptr<GU::Log::SeverityComponent> temp = std::dynamic_pointer_cast<SeverityComponent>(m_pimpl->m_data[i]);
+                            std::shared_ptr<GU::Log::SeverityComponent> temp2 = std::dynamic_pointer_cast<SeverityComponent>(component);
+                            (*temp) = (*temp2);
+                        }
+                        break;
+                        case GU::Log::componentId::TIME:
+                        {
+                            std::shared_ptr<GU::Log::TimeComponent> temp = std::dynamic_pointer_cast<TimeComponent>(m_pimpl->m_data[i]);
+                            std::shared_ptr<GU::Log::TimeComponent> temp2 = std::dynamic_pointer_cast<TimeComponent>(component);
+                            (*temp) = (*temp2);
+                        }
+                        break;
+                        case GU::Log::componentId::LINE:
+                            std::shared_ptr<GU::Log::LineComponent> temp = std::dynamic_pointer_cast<LineComponent>(m_pimpl->m_data[i]);
+                            std::shared_ptr<GU::Log::LineComponent> temp2 = std::dynamic_pointer_cast<LineComponent>(component);
+                            (*temp) = (*temp2);
+                        break;
+
+                    }
+                }
+
+            }
+        }
+
+
+        /*********************************************************************************//**
+        *	@brief  This method return a component with the given id.
+        *   @return A pointer to the log component with the given id. 
+        *************************************************************************************/
+        std::shared_ptr<LogComponent> LogFormatter::getComponent(const componentId &component) const
+        {
+            assert(m_pimpl != nullptr);
+            for(auto &element : m_pimpl->m_data)
+              if(element->m_id == component)
+                return element;
+
+            return nullptr;
+        }
+
+
+        /*********************************************************************************//**
+        *	@brief  This method will return a string formatted correctly for the log file. 
+        *   @return A string formatted correctly for the log file. 
+        *************************************************************************************/
+        GU::Core::String LogFormatter::format()
+        {
           assert(m_pimpl != nullptr);
           GU::Core::String temp;
 
@@ -55,78 +164,18 @@ namespace GU
             temp += "]";
           }
           return temp;
-      }
-
-      GU::Core::String LogFormatter::format(std::shared_ptr<GU::Log::LogEntry> entry)
-      {
-        UNUSED(entry);
-        assert(m_pimpl != nullptr);
-        return GU::Core::String("");
-      }
-
-
-      void LogFormatter::init(std::shared_ptr<GU::Log::LogComponent> component)
-      {
-        assert(m_pimpl != nullptr);
-        for(std::size_t i = 0; i < m_pimpl->m_data.size(); ++i)
-        {
-          if((*m_pimpl->m_data[i]) == (*component))
-          {
-            switch((*m_pimpl->m_data[i]).m_id)
-            {
-              case GU::Log::componentId::DATE:
-              {
-                std::shared_ptr<GU::Log::DateComponent> temp = std::dynamic_pointer_cast<DateComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::DateComponent> temp2 = std::dynamic_pointer_cast<DateComponent>(component);
-                (*temp) = (*temp2);
-              }
-              break;
-              case GU::Log::componentId::FILE:
-              {
-                std::shared_ptr<GU::Log::FileComponent> temp = std::dynamic_pointer_cast<FileComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::FileComponent> temp2 = std::dynamic_pointer_cast<FileComponent>(component);
-                (*temp) = (*temp2);
-              }
-              break;
-              case GU::Log::componentId::MSG:
-              {
-                std::shared_ptr<GU::Log::MsgComponent> temp = std::dynamic_pointer_cast<MsgComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::MsgComponent> temp2 = std::dynamic_pointer_cast<MsgComponent>(component);
-                (*temp) = (*temp2);
-              }
-              break;
-              case GU::Log::componentId::SEVERITY:
-              {
-                std::shared_ptr<GU::Log::SeverityComponent> temp = std::dynamic_pointer_cast<SeverityComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::SeverityComponent> temp2 = std::dynamic_pointer_cast<SeverityComponent>(component);
-                (*temp) = (*temp2);
-              }
-              break;
-              case GU::Log::componentId::TIME:
-              {
-                std::shared_ptr<GU::Log::TimeComponent> temp = std::dynamic_pointer_cast<TimeComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::TimeComponent> temp2 = std::dynamic_pointer_cast<TimeComponent>(component);
-                (*temp) = (*temp2);
-              }
-              break;
-              case GU::Log::componentId::LINE:
-                std::shared_ptr<GU::Log::LineComponent> temp = std::dynamic_pointer_cast<LineComponent>(m_pimpl->m_data[i]);
-                std::shared_ptr<GU::Log::LineComponent> temp2 = std::dynamic_pointer_cast<LineComponent>(component);
-                (*temp) = (*temp2);
-              break;
-
-            }
-          }
-
         }
-      }
 
-
-  		LogFormatter::~LogFormatter()
-  		{
+        
+        /*********************************************************************************//**
+        *	@brief  Destructor 
+        *************************************************************************************/
+        LogFormatter::~LogFormatter()
+        {
             assert(m_pimpl != nullptr);
             if(m_pimpl != nullptr)
                 delete m_pimpl;
-  		}
-	  }
+        } 
+    }
+
 }
