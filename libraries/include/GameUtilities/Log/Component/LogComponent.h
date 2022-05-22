18f02a6e980d7bd554c5735de47df86c25201bfa
Yanson <wlarson30@gmail.com>
2022-05-22 15:15:19 -0400

    Updated the formatting in LogComponent

--- libraries/include/GameUtilities/Log/Component/LogComponent.h
+++ libraries/include/GameUtilities/Log/Component/LogComponent.h
@@ -12,42 +12,43 @@ namespace GU
 {
     namespace Log
     {
-      struct LOG_EXPORT LogComponent
-      {
-        /*****************************************************************
-        * @brief: Constructor
-        * @param: id is a unique identifyer for the current object type
-        *****************************************************************/
-        LogComponent(const componentId &id);
+        struct LOG_EXPORT LogComponent
+        {
+            /*****************************************************************
+            * @brief: Constructor
+            * @param: id is a unique identifyer for the current object type
+            *****************************************************************/
+            LogComponent(const componentId &id);
 
 
-        /*****************************************************************
-        * @brief:  Equality operator
-        * @param:  comp will be used to check for equality against the
-        *          current LogComponent object.
-        * @return: True of the objects are equal and false otherwise
-        *****************************************************************/
-        bool operator == (const LogComponent &comp);
+            /*****************************************************************
+            * @brief:  Equality operator
+            * @param:  comp will be used to check for equality against the
+            *          current LogComponent object.
+            * @return: True of the objects are equal and false otherwise
+            *****************************************************************/
+            bool operator == (const LogComponent &comp);
 
 
-        /*****************************************************************
-        * @brief:  Equality operator
-        * @param:  comp will be used to check for equality against the
-        *          current componentId object.
-        * @return: True of the objects are equal and false otherwise
-        *****************************************************************/
-        bool operator == (const componentId &comp);
+            /*****************************************************************
+            * @brief:  Equality operator
+            * @param:  comp will be used to check for equality against the
+            *          current componentId object.
+            * @return: True of the objects are equal and false otherwise
+            *****************************************************************/
+            bool operator == (const componentId &comp);
 
 
-        /*****************************************************************
-        * @brief:  This method formats the data in the current object
-        *          and turns it into a string.
-        * @return: The formatted string.
-        *****************************************************************/
-        virtual GU::Core::String format() = 0;
+            /*****************************************************************
+            * @brief:  This method formats the data in the current object
+            *          and turns it into a string.
+            * @return: The formatted string.
+            *****************************************************************/
+            virtual GU::Core::String format() = 0;
 
-        const componentId m_id;            /// A unique identifyer for the component type
-      };
+            const componentId m_id;            /// A unique identifyer for the component type
+        };
+
     }
 }
 #endif
