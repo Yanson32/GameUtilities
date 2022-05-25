89c542b9775d4eeb1d60b5cff120c313da3e62dd
Yanson <wlarson30@gmail.com>
2022-05-25 00:19:48 -0400

    Updated  the projection function for the SAT algorithm

--- libraries/include/GameUtilities/Algorithm/SAT/SAT.h
+++ libraries/include/GameUtilities/Algorithm/SAT/SAT.h
@@ -3,14 +3,51 @@
 
 #include <utility>
 #include <Math/Vector2.h>
+#include <vector>
+#include "algorithm_export.h"
 
 namespace GU
 {
     namespace Al
     {
         class SatObject;
+        typedef std::vector<Math::Vector2<float>> PointsVec;
+        typedef Math::Vector2<float> Position;
+        typedef Math::Vector2<float> Direction;
+        typedef std::pair<float, float> Projection;
+        /****************************************************************************//**
+        *   @brief  This method projects a SatObject onto a direction vector. 
+        *   @param  first is the object to be projected.
+        *   @param  normal is the direction vector the object will be projected on.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection ALGORITHM_EXPORT project(const GU::Al::SatObject &first, const Direction &normal);        
+        
 
-        std::pair<float, float> project(const GU::Al::SatObject &first, Math::Vector2<float> normal);        
+        /****************************************************************************//**
+        *   @brief  This method projects a vector of points onto a direction vector. 
+        *           The points will be on global coordinates
+        *   @param  first is a vector of points to be projected onto the direction vector.
+        *   @param  normal is the direction vector the points will be projected onto.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection ALGORITHM_EXPORT project(const PointsVec &first, const Direction &normal);
+        
+
+        /****************************************************************************//**
+        *   @brief  This method projects a vector of points onto a direction vector. 
+        *           The points vector will be in local coordinates so an object position
+        *           will be needed.
+        *   @param  first is a vector of points to be projected onto the direction vector.
+        *   @param  firstPosition is the current position of the object represented by
+        *           the first PointsVec.
+        *   @param  normal is the direction vector the points will be projected onto.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection ALGORITHM_EXPORT project(const PointsVec &first, const Position &firstPosition, const Direction &normal);
     }
 }
 
--- libraries/src/GameUtilities/Algorithm/SAT/SAT.cpp
+++ libraries/src/GameUtilities/Algorithm/SAT/SAT.cpp
@@ -5,13 +5,75 @@ namespace GU
 {
     namespace Al
     {
-        std::pair<float, float> project(const GU::Al::SatObject &first, Math::Vector2<float> normal)
+        /****************************************************************************//**
+        *   @brief  This method projects a SatObject onto a direction vector. 
+        *   @param  first is the object to be projected.
+        *   @param  normal is the direction vector the object will be projected on.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection project(const GU::Al::SatObject &first, const Direction &normal)
         {
             std::pair<float, float> result(std::numeric_limits<float>::infinity(), -std::numeric_limits<float>::infinity());
             //Find the least and greatest projections of first onto edge
             for(std::size_t i = 0; i < first.getVertexCount(); ++i)
             {
                 float dotProduct = normal.dot(first.getGlobalCoordinate(i));
+                
+                if(dotProduct < result.first)
+                    result.first = dotProduct; 
+
+                if(dotProduct > result.second)
+                    result.second = dotProduct;
+            }
+            return result; 
+        }
+        
+
+        /****************************************************************************//**
+        *   @brief  This method projects a vector of points onto a direction vector. 
+        *           The points will be on global coordinates
+        *   @param  first is a vector of points to be projected onto the direction vector.
+        *   @param  normal is the direction vector the points will be projected onto.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection project(const PointsVec &first, const Direction &normal)
+        {
+            std::pair<float, float> result(std::numeric_limits<float>::infinity(), -std::numeric_limits<float>::infinity());
+            //Find the least and greatest projections of first onto edge
+            for(std::size_t i = 0; i < first.size(); ++i)
+            {
+                float dotProduct = normal.dot(first[i]);
+                
+                if(dotProduct < result.first)
+                    result.first = dotProduct; 
+
+                if(dotProduct > result.second)
+                    result.second = dotProduct;
+            }
+            return result; 
+        }
+        
+        
+        /****************************************************************************//**
+        *   @brief  This method projects a vector of points onto a direction vector. 
+        *           The points vector will be in local coordinates so an object position
+        *           will be needed.
+        *   @param  first is a vector of points to be projected onto the direction vector.
+        *   @param  firstPosition is the current position of the object represented by
+        *           the first PointsVec.
+        *   @param  normal is the direction vector the points will be projected onto.
+        *   @return A std::pair where the first data member is the lowest projection
+        *           and the second data member is the highest projection. 
+        ********************************************************************************/
+        Projection project(const PointsVec &first, const Position &firstPosition, const Direction &normal)
+        {
+            std::pair<float, float> result(std::numeric_limits<float>::infinity(), -std::numeric_limits<float>::infinity());
+            //Find the least and greatest projections of first onto edge
+            for(std::size_t i = 0; i < first.size(); ++i)
+            {
+                float dotProduct = normal.dot(first[i] + firstPosition);
                 
                 if(dotProduct < result.first)
                     result.first = dotProduct; 
