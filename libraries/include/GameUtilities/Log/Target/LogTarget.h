ca97f42eded2c93bd360488e7373d0847c102ea6
Yanson <wlarson30@gmail.com>
2022-05-31 18:36:03 -0400

    Updated the log target with comments

--- libraries/include/GameUtilities/Log/Target/LogTarget.h
+++ libraries/include/GameUtilities/Log/Target/LogTarget.h
@@ -1,5 +1,10 @@
 #ifndef GU_LOG_TARGET_H
 #define GU_LOG_TARGET_H
+/*********************************************************************************//**
+*	@author	        Wayne J Larson Jr.
+*	@date 	        5/30/22
+*	@description    This is the base class for all Log Targets. 
+*************************************************************************************/
 
 #include "log_export.h"
 #include "GameUtilities/Core/String.h"
@@ -11,9 +16,33 @@ namespace GU
   		class LOG_EXPORT LogTarget
   		{
   			public:
+                /*********************************************************************************//**
+                *	@brief  Constructor 
+                *************************************************************************************/
   				LogTarget();
+                
+
+                /*********************************************************************************//**
+                *	@brief  This method writes a string to the log target.
+                *   @param  msg is the string to be written. 
+                *************************************************************************************/
   				virtual void write(const GU::Core::String &msg) = 0;
-  				bool operator == (const LogTarget &logTarget) const;
+                
+
+                /*********************************************************************************//**
+                *	@brief  This method retruns true if the current log target and the passed log
+                *           target are equal.
+                *   @param  logTarget is the log target the current log target will be checked against
+                *           for equality.
+                *   @return True if the current log target and passed log target are equal and false
+                *           otherwise. 
+                *************************************************************************************/
+                bool operator == (const LogTarget &logTarget) const;
+                
+
+                /*********************************************************************************//**
+                *	@brief  Destructor 
+                *************************************************************************************/
   				virtual ~LogTarget();
 
   		};
--- libraries/src/GameUtilities/Log/Target/LogTarget.cpp
+++ libraries/src/GameUtilities/Log/Target/LogTarget.cpp
@@ -5,21 +5,35 @@ namespace GU
 {
     namespace Log
     {
+        /*********************************************************************************//**
+        *	@brief  Constructor 
+        *************************************************************************************/
   		LogTarget::LogTarget()
   		{
 
   		}
 
+        
+        /*********************************************************************************//**
+        *	@brief  This method retruns true if the current log target and the passed log
+        *           target are equal.
+        *   @param  logTarget is the log target the current log target will be checked against
+        *           for equality.
+        *   @return True if the current log target and passed log target are equal and false
+        *           otherwise. 
+        *************************************************************************************/
   		bool LogTarget::operator == (const LogTarget &logTarget) const
   		{
-        UNUSED(logTarget);
-  			return true;
+  			return (*this)==logTarget;
   		}
 
 
+        /*********************************************************************************//**
+        *	@brief  Destructor 
+        *************************************************************************************/
   		LogTarget::~LogTarget()
   		{
 
-  		}
-	 }
+        }
+    }
 }
