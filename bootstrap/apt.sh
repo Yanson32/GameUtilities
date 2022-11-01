f97f6ffe14d02671a07ebd3d8e9b796e8eee9756
Yanson <wlarson30@gmail.com>
2022-11-01 14:07:52 -0400

    Updated bootstrap script for apt package manager

--- bootstrap/apt.sh
+++ bootstrap/apt.sh
@@ -1,3 +1,8 @@
 sudo apt-get update -y
 sudo apt install graphviz -y
 sudo apt install libsfml-dev -y
+sudo apt install cmake -y
+sudo apt install build-essential -y
+sudo apt install git -y
+sudo apt install libx11-dev -y
+
