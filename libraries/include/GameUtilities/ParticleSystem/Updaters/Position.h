b9a4febcc67c719f0d4dcbdf779079eadce5e105
Yanson <wlarson30@gmail.com>
2022-11-03 06:26:29 -0400

    Added documentation for the particle system Position updater.

--- libraries/include/GameUtilities/ParticleSystem/Updaters/Position.h
+++ libraries/include/GameUtilities/ParticleSystem/Updaters/Position.h
@@ -1,24 +1,62 @@
 #ifndef PARTICLE_SYSTEM_UPDATER_POSITION_H
 #define PARTICLE_SYSTEM_UPDATER_POSITION_H
+/**************************************************************************
+*   @Author     Wayne J Larson Jr.
+*   @Date       4/08/18
+*   @class      This updater class is used to update the particles position.
+*   @file       Position.h
+**************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "particlesystem_export.h"
 #include "GameUtilities/ParticleSystem/Updaters/Base.h"
 
 namespace GU
 {
+
     namespace PS
     {
+
         namespace UP
         {
+
             class PARTICLESYSTEM_EXPORT Position: public GU::PS::UP::Base
             {
                 public:
+                    /************************************************************************//**
+                    *   @brief  Constructor 
+                    ****************************************************************************/
                     Position();
+                    
+
+                    /************************************************************************//**
+                    *   @brief  This method updates the current position of the particle.
+                    *   @param  manager is a reference to the attrubute manager.
+                    *   @param  deltaTime is the length of time for a single frame 
+                    ****************************************************************************/
                     virtual void update(GU::PS::AT::Manager &manager, const float &deltaTime);
+           
+ 
+                    /************************************************************************//**
+                    *   @brief  Destructor 
+                    ****************************************************************************/
                     virtual ~Position();
-
-                protected:
-
-                private:
             };
         }
     }
--- libraries/src/GameUtilities/ParticleSystem/Updaters/Position.cpp
+++ libraries/src/GameUtilities/ParticleSystem/Updaters/Position.cpp
@@ -1,20 +1,57 @@
+/**************************************************************************
+*   @Author     Wayne J Larson Jr.
+*   @Date       4/08/18
+*   @class      This updater class is used to update the particles position.
+*   @file       Position.h
+**************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/ParticleSystem/Updaters/Position.h"
 #include "GameUtilities/ParticleSystem/Attributes/Id.h"
 #include "GameUtilities/ParticleSystem/Attributes/Manager.h"
 #include "GameUtilities/ParticleSystem/Attributes/Velocity.h"
 #include "GameUtilities/ParticleSystem/Attributes/Position.h"
 #include <iostream>
+
 namespace GU
 {
+
     namespace PS
     {
+
         namespace UP
         {
+
+            /************************************************************************//**
+            *   @brief  Constructor 
+            ****************************************************************************/
             Position::Position()
             {
                 //ctor
             }
 
+
+            /************************************************************************//**
+            *   @brief  This method updates the current position of the particle.
+            *   @param  manager is a reference to the attrubute manager.
+            *   @param  deltaTime is the length of time for a single frame 
+            ****************************************************************************/
             void Position::update(GU::PS::AT::Manager &manager, const float &deltaTime)
             {
                 typedef GU::PS::AT::Velocity Velocity; 
@@ -34,6 +71,10 @@ namespace GU
 
             }
 
+
+            /************************************************************************//**
+            *   @brief  Destructor 
+            ****************************************************************************/
             Position::~Position()
             {
                 //dtor
