01a2b0d1af988df8722cde330a34aaf513e1e7f2
Yanson <wlarson30@gmail.com>
2022-11-08 04:28:10 -0500

    Updated the comments on Starburst.h class

--- libraries/include/GameUtilities/ParticleSystem/Systems/Starburst.h
+++ libraries/include/GameUtilities/ParticleSystem/Systems/Starburst.h
@@ -1,5 +1,30 @@
 #ifndef GU_ATTRIBUTES_STARBURST_H
 #define GU_ATTRIBUTES_STARBURST_H
+/**************************************************************************
+*   @Author     Wayne J Larson Jr.
+*   @Date       04/06/19
+*   @class      This class creates a particle system in which all the
+*               particles start at a point and radiate out from the middle. 
+*   @file       StarBurst.h
+**************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 
 #include "particlesystem_export.h"
 //#include "GameUtilities/ParticleSystem/Systems/ParticleSystem.h"
@@ -32,14 +57,19 @@ namespace GU
                     ****************************************************************/
                     virtual void update(const float &deltaTime);
 
+
+                    /************************************************************//**
+                    *   @brief  Initialize the particle system. 
+                    ****************************************************************/
 					virtual void init();
+
 
                     /************************************************************//**
                     *   @brief  Update the magnitude of the velocity vectors.
                     *   @param  magnitude the new magnitude of the vectors.
                     ****************************************************************/
                     void setMagnitude(const unsigned magnitude);
-
+                public:
 					std::size_t m_size = 10;
                     Math::Vector2<float> m_position;
             };
--- libraries/src/GameUtilities/ParticleSystem/Systems/Starburst.cpp
+++ libraries/src/GameUtilities/ParticleSystem/Systems/Starburst.cpp
@@ -1,3 +1,28 @@
+/**************************************************************************
+*   @Author     Wayne J Larson Jr.
+*   @Date       04/06/19
+*   @class      This class creates a particle system in which all the
+*               particles start at a point and radiate out from the middle. 
+*   @file       StarBurst.h
+**************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/ParticleSystem/Systems/Starburst.h"
 //#include "GameUtilities/ParticleSystem/Emitters/PointEmitter.h"
 #include "GameUtilities/ParticleSystem/Attributes/Position.h"
@@ -18,6 +43,7 @@ namespace GU
     {
         namespace SY
         {
+            
             /**********************************************************//**
             *   @brief  Constructor.
             *   @param  position the base position of the particle system
@@ -45,6 +71,10 @@ namespace GU
                 GU::PS::UP::UpdateManager::update(*this, deltaTime);
             }
 
+            
+            /************************************************************//**
+            *   @brief  Initialize the particle system. 
+            ****************************************************************/
 			void Starburst::init()
 			{
 				std::shared_ptr<GU::PS::AT::Position> pos(new GU::PS::AT::Position(*this, m_size));
@@ -87,6 +117,8 @@ namespace GU
                 std::shared_ptr<GU::PS::UP::UpdaterBase> up(new GU::PS::UP::Position());
                 this->addUpdater(up);
 			}
+
+
             /************************************************************//**
             *   @brief  Update the magnitude of the velocity vectors.
             *   @param  magnitude the new magnitude of the vectors.
