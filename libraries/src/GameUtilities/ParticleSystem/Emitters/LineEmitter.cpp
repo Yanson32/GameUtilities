d5ed55a746779bdfaac43eef20e37cdd398658f0
Yanson <wlarson30@gmail.com>
2022-11-08 04:03:03 -0500

    Added comments to LineEmitter class

--- libraries/src/GameUtilities/ParticleSystem/Emitters/LineEmitter.cpp
+++ libraries/src/GameUtilities/ParticleSystem/Emitters/LineEmitter.cpp
@@ -7,12 +7,23 @@ namespace GU
     {
         namespace EM
         {
+            
+            /************************************************************************//**
+            *   @brief  Constructor. 
+            *   @param  parent is a pointer to the parent particle system. 
+            *   @param  ppf (particles per frame) is the number of particles in the system.
+            *   @param  relativePosition is the relative position of the emitter. 
+            ****************************************************************************/
             LineEmitter::LineEmitter(GU::PS::ParticleSystem *parent, const std::size_t ppf, Math::Vector relativePosition):
             Emitter(parent, ppf, relativePosition)
             {
                 //ctor
             }
 
+            
+            /************************************************************************//**
+            *   @brief  Destructor.
+            ****************************************************************************/
             LineEmitter::~LineEmitter()
             {
                 //dtor
