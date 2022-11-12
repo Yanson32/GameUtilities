80255c7b184d69c5809d0c27cf71764b4d416975
Yanson <wlarson30@gmail.com>
2022-11-11 21:13:33 -0500

    Updated documentation for LogFileTarget

--- libraries/src/GameUtilities/Log/Target/LogFileTarget.cpp
+++ libraries/src/GameUtilities/Log/Target/LogFileTarget.cpp
@@ -1,3 +1,28 @@
+/*********************************************************************************//**
+*	@author	Wayne J Larson Jr.
+*	@date 	5/27/22
+*   @class  LogFileTarget
+*   @file   LogFileTarget.cpp
+*	@brief  This class defines a log target that will write logs to a file 
+*************************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "log_export.h"
 #include "GameUtilities/Log/Target/LogFileTarget.h"
 #include <iostream>
