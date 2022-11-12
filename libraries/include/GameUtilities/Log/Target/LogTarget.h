7458c2635709a39749ab0107ad81e718f7cf80b4
Yanson <wlarson30@gmail.com>
2022-11-11 21:11:22 -0500

    Updated the documentation for LogTarget

--- libraries/include/GameUtilities/Log/Target/LogTarget.h
+++ libraries/include/GameUtilities/Log/Target/LogTarget.h
@@ -1,14 +1,34 @@
 #ifndef GU_LOG_TARGET_H
 #define GU_LOG_TARGET_H
 /*********************************************************************************//**
-*	@author	        Wayne J Larson Jr.
-*	@date 	        5/30/22
-*	@description    This is the base class for all Log Targets. 
+*	@author	 Wayne J Larson Jr.
+*	@date 	 5/30/22
+*   @class   LogTarget
+*   @file    LogTarget.cpp
+*	@brief   This is the base class for all Log Targets. 
 *************************************************************************************/
 
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "log_export.h"
 #include "GameUtilities/Core/String.h"
 #include <iostream>
+
 namespace GU
 {
     namespace Log
--- libraries/src/GameUtilities/Log/Target/LogTarget.cpp
+++ libraries/src/GameUtilities/Log/Target/LogTarget.cpp
@@ -1,3 +1,28 @@
+/*********************************************************************************//**
+*	@author	 Wayne J Larson Jr.
+*	@date 	 5/30/22
+*   @class   LogTarget
+*   @file    LogTarget.cpp
+*	@brief   This is the base class for all Log Targets. 
+*************************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Log/Target/LogTarget.h"
 #include <iostream>
 #include "GameUtilities/Core/Macros.h"
