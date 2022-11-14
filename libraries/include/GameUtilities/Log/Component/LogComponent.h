16b756f20ff992bc83844f720d824325318a9b62
Yanson <wlarson30@gmail.com>
2022-11-14 06:16:54 -0500

    Updated documentation for the LogComponent class

--- libraries/include/GameUtilities/Log/Component/LogComponent.h
+++ libraries/include/GameUtilities/Log/Component/LogComponent.h
@@ -1,9 +1,11 @@
 #ifndef GU_LOG_COMPONENT_H
 #define GU_LOG_COMPONENT_H
 /*********************************************************************************//**
-*	@author	Wayne J Larson Jr.
-* @date 4/8/2022
-* @description This class is used as a base class for all log components.
+*   @author	Wayne J Larson Jr.
+*   @date   4/8/2022
+*   @class  LogComponent
+*   @file   LogComponent.h
+*   @brief  This class is used as a base class for all log components.
 *************************************************************************************/
 #include "log_export.h"
 #include "GameUtilities/Core/String.h"
@@ -14,57 +16,57 @@ namespace GU
     {
         struct LOG_EXPORT LogComponent
         {
-            /*****************************************************************
-            * @brief: Constructor
-            * @param: id is a unique identifyer for the current object type
+            /*************************************************************//**
+            * @brief Constructor
+            * @param id is a unique identifyer for the current object type
             *****************************************************************/
             LogComponent(const componentId &id);
 
 
-            /*****************************************************************
-            * @brief:  Equality operator
-            * @param:  comp will be used to check for equality against the
-            *          current LogComponent object.
-            * @return: True of the objects are equal and false otherwise
+            /*************************************************************//**
+            * @brief  Equality operator
+            * @param  comp will be used to check for equality against the
+            *         current LogComponent object.
+            * @return True of the objects are equal and false otherwise
             *****************************************************************/
             bool operator == (const LogComponent &comp);
 
 
-            /*****************************************************************
-            * @brief:  Equality operator
-            * @param:  comp will be used to check for equality against the
-            *          current componentId.
-            * @return: True of the objects are equal and false otherwise
+            /*************************************************************//**
+            * @brief  Equality operator
+            * @param  comp will be used to check for equality against the
+            *         current componentId.
+            * @return True of the objects are equal and false otherwise
             *****************************************************************/
             bool operator == (const componentId &comp);
 
 
-            /*****************************************************************
-            * @brief:  Not equal operator
-            * @param:  comp will be used to check for equality against the
-            *          current LogComponent object.
-            * @return: True of the objects are not equal and false otherwise
+            /*************************************************************//**
+            * @brief  Not equal operator
+            * @param  comp will be used to check for equality against the
+            *         current LogComponent object.
+            * @return True of the objects are not equal and false otherwise
             *****************************************************************/
             bool operator != (const LogComponent &comp);
             
 
-            /*****************************************************************
-            * @brief:  Not equal operator
-            * @param:  comp will be used to check for equality against the
-            *          current componentId.
-            * @return: True of the objects are not equal and false otherwise
+            /*************************************************************//**
+            * @brief  Not equal operator
+            * @param  comp will be used to check for equality against the
+            *         current componentId.
+            * @return True of the objects are not equal and false otherwise
             *****************************************************************/
             bool operator != (const componentId &comp);
             
 
-            /*****************************************************************
-            * @brief:  This method formats the data in the current object
-            *          and turns it into a string.
-            * @return: The formatted string.
+            /*************************************************************//**
+            * @brief  This method formats the data in the current object
+            *         and turns it into a string.
+            * @return The formatted string.
             *****************************************************************/
             virtual GU::Core::String format() = 0;
 
-            const componentId m_id;            /// A unique identifyer for the component type
+            const componentId m_id;            /**< A unique identifyer for the component type */
         };
 
     }
--- libraries/src/GameUtilities/Log/Component/LogComponent.cpp
+++ libraries/src/GameUtilities/Log/Component/LogComponent.cpp
@@ -1,12 +1,19 @@
+/*********************************************************************************//**
+*   @author	Wayne J Larson Jr.
+*   @date   4/8/2022
+*   @class  LogComponent
+*   @file   LogComponent.cpp
+*   @brief  This class is used as a base class for all log components.
+*************************************************************************************/
 #include "GameUtilities/Log/Component/LogComponent.h"
 
 namespace GU
 {
     namespace Log
     {
-        /*****************************************************************
-        * @brief: Constructor
-        * @param: id is a unique identifyer for the current object type
+        /*************************************************************//**
+        * @brief Constructor
+        * @param id is a unique identifyer for the current object type
         *****************************************************************/
         LogComponent::LogComponent(const componentId &id):
         m_id(id)
@@ -14,11 +21,11 @@ namespace GU
 
         }
 
-        /*****************************************************************
-        * @brief:  Equality operator
-        * @param:  comp will be used to check for equality against the
-        *          current LogComponent object.
-        * @return: True of the objects are equal and false otherwise
+        /*************************************************************//**
+        * @brief  Equality operator
+        * @param  comp will be used to check for equality against the
+        *         current LogComponent object.
+        * @return True of the objects are equal and false otherwise
         *****************************************************************/
         bool LogComponent::operator == (const LogComponent &comp)
         {
@@ -26,11 +33,11 @@ namespace GU
         }
 
 
-        /*****************************************************************
-        * @brief:  Equality operator
-        * @param:  comp will be used to check for equality against the
-        *          current componentId.
-        * @return: True of the objects are equal and false otherwise
+        /*************************************************************//**
+        * @brief  Equality operator
+        * @param  comp will be used to check for equality against the
+        *         current componentId.
+        * @return True of the objects are equal and false otherwise
         *****************************************************************/
         bool LogComponent::operator == (const componentId &comp)
         {
@@ -38,11 +45,11 @@ namespace GU
         }
 
         
-        /*****************************************************************
-        * @brief:  Not equal operator
-        * @param:  comp will be used to check for equality against the
-        *          current LogComponent object.
-        * @return: True of the objects are not equal and false otherwise
+        /*************************************************************//**
+        * @brief  Not equal operator
+        * @param  comp will be used to check for equality against the
+        *         current LogComponent object.
+        * @return True of the objects are not equal and false otherwise
         *****************************************************************/
         bool LogComponent::operator != (const LogComponent &comp)
         {
@@ -50,11 +57,11 @@ namespace GU
         }
 
 
-        /*****************************************************************
-        * @brief:  Not equal operator
-        * @param:  comp will be used to check for equality against the
-        *          current componentId.
-        * @return: True of the objects are not equal and false otherwise
+        /*************************************************************//**
+        * @brief  Not equal operator
+        * @param  comp will be used to check for equality against the
+        *         current componentId.
+        * @return True of the objects are not equal and false otherwise
         *****************************************************************/
         bool LogComponent::operator != (const componentId &comp)
         {
