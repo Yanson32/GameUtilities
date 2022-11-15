fdf237c81966f5a8c1df38fba0c4ae0e2dac2c9e
Yanson <wlarson30@gmail.com>
2022-11-15 04:36:10 -0500

    Updated the documentation for the LineComponent class

--- libraries/include/GameUtilities/Log/Component/LineComponent.h
+++ libraries/include/GameUtilities/Log/Component/LineComponent.h
@@ -2,11 +2,30 @@
 #define GU_LOG_LINECOMPONENT_H
 /*********************************************************************************//**
 *	@author	Wayne J Larson Jr.
-* @date 4/8/2022
-* @description This class defines a LogComponent that represents the line number
-*              where the log message originated from.
+*   @date   4/8/2022
+*   @class  LineComponent
+*   @file   LineComponent.h
+*   @brief  This class defines a LogComponent that represents the line number
+*           where the log message originated from.
 *************************************************************************************/
 
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "log_export.h"
 #include "GameUtilities/Core/String.h"
 #include "GameUtilities/Log/Component/LogComponent.h"
@@ -19,35 +38,35 @@ namespace GU
     {
       struct LOG_EXPORT LineComponent: public LogComponent
       {
-        /*****************************************************************
-        * @brief: The constructor
+        /*************************************************************//**
+        * @brief The constructor
         *****************************************************************/
         LineComponent();
 
 
-        /*****************************************************************
-        * @brief: The constructor
-        * @param: line the line number where the log message originated from
+        /*************************************************************//**
+        * @brief The constructor
+        * @param line the line number where the log message originated from
         *****************************************************************/
         LineComponent(const int &line);
 
 
-        /*****************************************************************
-        * @brief:  Assignment operator overload.
-        * @param:  comp contains the values to be assigned to the current
-        *          object.
-        * @return: A reference to the updated current object.
+        /*************************************************************//**
+        * @brief  Assignment operator overload.
+        * @param  comp contains the values to be assigned to the current
+        *         object.
+        * @return A reference to the updated current object.
         *****************************************************************/
         LineComponent& operator = (const LineComponent &comp);
 
 
-        /*****************************************************************
-        * @brief:  This method creates a string from the current line number.
-        * @return: A string representing the current line number.
+        /*************************************************************//**
+        * @brief  This method creates a string from the current line number.
+        * @return A string representing the current line number.
         *****************************************************************/
         virtual GU::Core::String format() override;
     public:
-        int m_line = -1;
+        int m_line = -1;    /**< The line in source code where the log message originated */
       };
     }
 }
