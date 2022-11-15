eed7e3e955987b69d5630bec3cdd0ee30a5a0928
Yanson <wlarson30@gmail.com>
2022-11-15 04:24:13 -0500

    Updated documentation for SeverityComponent

--- libraries/include/GameUtilities/Log/Component/SeverityComponent.h
+++ libraries/include/GameUtilities/Log/Component/SeverityComponent.h
@@ -1,11 +1,30 @@
 #ifndef GU_LOG_SECURITYCOMPONENT_H
 #define GU_LOG_SECURITYCOMPONENT_H
 /*********************************************************************************//**
-*	@author	Wayne J Larson Jr.
-* @date 4/8/2022
-* @description This class defines the severity of a log message.
+*   @author	Wayne J Larson Jr.
+*   @date   4/8/2022
+*   @class  SeverityComponent
+*   @file   SeverityComponent.h
+*   @brief  This class defines the severity of a log message.
 *************************************************************************************/
 
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "log_export.h"
 #include "GameUtilities/Core/String.h"
 #include "GameUtilities/Log/Component/LogComponent.h"
@@ -18,35 +37,35 @@ namespace GU
     {
         struct LOG_EXPORT SeverityComponent: public LogComponent
         {
-            /*****************************************************************
-            * @brief: The constructor
+            /*************************************************************//**
+            * @brief The constructor
             *****************************************************************/
             SeverityComponent();
 
 
-            /*****************************************************************
-            * @brief: The constructor
-            * @param: severity indicates how important the log is.
+            /*************************************************************//**
+            * @brief The constructor
+            * @param severity indicates how important the log is.
             *****************************************************************/
             SeverityComponent(const LogType &severity);
 
 
-            /*****************************************************************
-            * @brief:  Assignment operator overload.
-            * @param:  comp contains the values to be assigned to the current
-            *          object.
-            * @return: A reference to the updated current object.
+            /*************************************************************//**
+            * @brief  Assignment operator overload.
+            * @param  comp contains the values to be assigned to the current
+            *         object.
+            * @return A reference to the updated current object.
             *****************************************************************/
             SeverityComponent& operator = (const SeverityComponent &comp);
 
 
-            /*****************************************************************
-            * @brief:  This method creates a string from the current severity.
-            * @return: A string representing the current severity object.
+            /*************************************************************//**
+            * @brief  This method creates a string from the current severity.
+            * @return A string representing the current severity object.
             *****************************************************************/
             GU::Core::String format();
 
-            LogType m_severity = LogType::GU_MESSAGE;           //The logs severity
+            LogType m_severity = LogType::GU_MESSAGE;           /**< The logs severity */
         };
     }
 }
