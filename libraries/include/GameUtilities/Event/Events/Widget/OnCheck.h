0082ae1fbd8dc3c34f3d81ad3999ff7897d4aa1d
Yanson <wlarson30@gmail.com>
2022-11-20 16:46:45 -0500

    Updated the documentation for the OnCheck event

--- libraries/include/GameUtilities/Event/Events/Widget/OnCheck.h
+++ libraries/include/GameUtilities/Event/Events/Widget/OnCheck.h
@@ -1,10 +1,10 @@
 #ifndef GAMEUTILITIES_ON_CHECK_H 
 #define GAMEUTILITIES_ON_CHECK_H 
-/**************************************************************************
-*   @Author:     Wayne J Larson Jr.
-*   @Date:       5/16/22
-*   @Purpose:    This event is generated when the user clicks on something. 
-**************************************************************************/
+/**************************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   5/16/22
+*   @file   OnCheck.h
+******************************************************************************/
 
 /*************************************************************************
 *                           COPYRIGHT NOTICE
@@ -31,22 +31,30 @@ namespace GU
 {
     namespace Evt
     {
+        /**************************************************************************//**
+        *   @class OnCheck 
+        *   @brief This event is generated when a checkbox is checked or unchecked
+        ******************************************************************************/
         class EVENT_EXPORT OnCheck: public WidgetEvent
         {
             public:
-                /**************************************************************************
-                *   @breif  Constructor
-                *   @param  checkBoxId is a unique identifier for the checkbox.
-                *   @param  checked is true if the checkbox was checked and false otherwise.
+                /**********************************************************************//**
+                *   @brief  Constructor
+                *   @param  parent is the widget where the event originated from.
+                *   @param  widgetId is the id of the widget that generated the event.
+                *   @param  checked is true if the checkbox is checked and false otherwise
+                *   @param  line is the line of source code that generated the event.
+                *   @param  file is the source code file that generated the event.
                 **************************************************************************/
                 OnCheck(std::shared_ptr<void> parent, const int &widgetId, const bool &checked, const int &line = -1, const char* file = "");
                 
 
-                /**************************************************************************
-                *   @breif  Destructor 
+                /**********************************************************************//**
+                *   @brief  Destructor 
                 **************************************************************************/
                 virtual ~OnCheck();
             public: 
+                ///This varialbe is true when the checkbox is checked and false otherwise
                 const bool m_checked;
 
         };
--- libraries/src/GameUtilities/Event/Events/Widget/OnCheck.cpp
+++ libraries/src/GameUtilities/Event/Events/Widget/OnCheck.cpp
@@ -1,13 +1,39 @@
+/**************************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   5/16/22
+*   @file   OnCheck.h
+******************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Event/Events/Widget/OnCheck.h"
 #include "GameUtilities/Event/EventId.h"
 namespace GU
 {
     namespace Evt
     {
-        /**************************************************************************
-        *   @breif  Constructor
-        *   @param  checkBoxId is a unique identifier for the checkbox.
-        *   @param  checked is true if the checkbox was checked and false otherwise.
+        /**********************************************************************//**
+        *   @brief  Constructor
+        *   @param  parent is the widget where the event originated from.
+        *   @param  widgetId is the id of the widget that generated the event.
+        *   @param  checked is true if the checkbox is checked and false otherwise
+        *   @param  line is the line of source code that generated the event.
+        *   @param  file is the source code file that generated the event.
         **************************************************************************/
         OnCheck::OnCheck(std::shared_ptr<void> parent, const int &widgetId, const bool &checked, const int &line, const char* file):
         WidgetEvent(parent, widgetId, static_cast<int>(GU::Evt::EventId::ON_CHECK), line, file),
@@ -17,8 +43,8 @@ namespace GU
         }
 
         
-        /**************************************************************************
-        *   @breif  Destructor 
+        /**********************************************************************//**
+        *   @brief  Destructor 
         **************************************************************************/
         OnCheck::~OnCheck()
         {
