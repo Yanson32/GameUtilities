d08e7204ae1d327992bf534275b7229e924f4528
Yanson <wlarson30@gmail.com>
2022-11-22 02:21:42 -0500

    Updated the documentation for the OnStart event

--- libraries/src/GameUtilities/Event/Events/OnStart.cpp
+++ libraries/src/GameUtilities/Event/Events/OnStart.cpp
@@ -1,8 +1,26 @@
-/**************************************************************************
-*   Author:     Wayne J Larson Jr.
-*   Date:       10/01/22
-*   Purpose:    This event is generates when something is started. 
+/**********************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   10/01/22
+*   @file   OnStart.h
 **************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Event/Events/OnStart.h"
 #include "event_export.h"
 #include "GameUtilities/Event/EventId.h"
@@ -13,8 +31,11 @@ namespace GU
     namespace Evt
     {
 
-        /**************************************************************************
-        *   Constructor
+        /**********************************************************************//**
+        *   @brief  Constructor.
+        *   @param  startId is a unique id for the entity to be started. 
+        *   @param  line is the source code line number that generated the event.
+        *   @param  file is the source code file that generated the event
         **************************************************************************/
         OnStart::OnStart(const int &startId, const int &line, const char* file):
         Event(static_cast<int>(GU::Evt::EventId::ON_START), line, file),
@@ -24,8 +45,8 @@ namespace GU
         }
 
         
-        /**************************************************************************
-        *   Destructor
+        /**********************************************************************//**
+        *   @brief  Destructor
         **************************************************************************/
         OnStart::~OnStart()
         {
