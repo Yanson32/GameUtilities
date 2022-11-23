588f13e5dc781c0ae8de0cc99cb4a3eb06e3ea72
Yanson <wlarson30@gmail.com>
2022-11-22 20:50:27 -0500

    Updated the documentation

--- libraries/src/GameUtilities/Event/Events/OnUpdate.cpp
+++ libraries/src/GameUtilities/Event/Events/OnUpdate.cpp
@@ -3,6 +3,24 @@
 *   @date   10/01/22
 *   @file   OnUpdate.cpp
 **************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Event/Events/OnUpdate.h"
 #include "event_export.h"
 #include "GameUtilities/Event/EventId.h"
