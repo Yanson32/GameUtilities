d424a6f7684c74b80b9a0091cccf964119a0ce35
Yanson <wlarson30@gmail.com>
2022-11-23 22:33:43 -0500

    Updated the documentation for the OnPop event

--- libraries/include/GameUtilities/Event/Events/OnPop.h
+++ libraries/include/GameUtilities/Event/Events/OnPop.h
@@ -1,10 +1,9 @@
 #ifndef GAMEUTILITIES_ONPOP_H
 #define GAMEUTILITIES_ONPOP_H
-/**************************************************************************
-*   @Author:     Wayne J Larson Jr.
-*   @Date:       05/12/22
-*   @Purpose:    This class is a subclass of the Event class. This event
-*               is generated when something should be poped of a stack.  
+/**********************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   05/12/22
+*   @file   OnPop.h
 **************************************************************************/
 
 /*************************************************************************
@@ -32,24 +31,29 @@ namespace GU
     namespace Evt
     {
 
+        /**********************************************************************//**
+        *   @class  OnPop
+        *   @brief  This class is a subclass of the Event class. This event
+        *           is generated when something should be poped of a stack.  
+        **************************************************************************/
 		class EVENT_EXPORT OnPop: public Event
 		{
 			public:
-                /**************************************************************************
+                /**********************************************************************//**
                 *   @brief  Constructor
-                *   @param  id is the unique identifier for the pop event.
+                *   @param  popId is the unique identifier for the pop event.
                 *   @param  line is the line number where the event originated from
                 *   @param  file is the file where the event originated from
                 **************************************************************************/
 				OnPop(const int &popId = -1, const int &line = 0, const char* file = "");
                 
 
-                /**************************************************************************
+                /**********************************************************************//**
                 *   @brief  Deconstructor 
                 **************************************************************************/
 				virtual ~OnPop();
 	        public:
-                const int m_popId;
+                const int m_popId;      /**< A unique identifier for the entity to be poped. */
 
         };
 	}
