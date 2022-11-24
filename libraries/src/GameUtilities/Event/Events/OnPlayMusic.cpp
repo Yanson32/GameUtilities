a67f084fa345dd233cdcc3dea6413f54b55568fe
Yanson <wlarson30@gmail.com>
2022-11-23 22:46:59 -0500

    Updated the documentation for the OnPlayMusic event

--- libraries/src/GameUtilities/Event/Events/OnPlayMusic.cpp
+++ libraries/src/GameUtilities/Event/Events/OnPlayMusic.cpp
@@ -1,3 +1,26 @@
+/**********************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   5/11/22
+*   @file   OnPlayMusic.cpp
+**************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Event/Events/OnPlayMusic.h"
 #include "GameUtilities/Event/EventId.h"
 
@@ -5,7 +28,7 @@ namespace GU
 {
     namespace Evt
     {
-        /**************************************************************************
+        /**********************************************************************//**
         *   @brief  Constructor.
         *   @param  musicFile is the music to be played.   
         *   @param  line is the line in code where the event was generated.
@@ -20,7 +43,7 @@ namespace GU
 
         
 
-        /**************************************************************************
+        /**********************************************************************//**
         *   @brief  Destructor.
         **************************************************************************/
 		OnPlayMusic::~OnPlayMusic()
