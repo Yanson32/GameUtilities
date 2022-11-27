43e1a73cff8f6db86fac997d72de78170029582b
Yanson <wlarson30@gmail.com>
2022-11-26 22:23:11 -0500

    Updated the documentation for the OnContextMenu event

--- libraries/include/GameUtilities/Event/Events/Mouse/OnContextMenu.h
+++ libraries/include/GameUtilities/Event/Events/Mouse/OnContextMenu.h
@@ -1,10 +1,9 @@
 #ifndef GAMEUTILITIES_EVNT_MOUSE_ON_CONTEXTMENU_H
 #define GAMEUTILITIES_EVNT_MOUSE_ON_CONTEXTMENU_H
-/**************************************************************************
-*   Author:     Wayne J Larson Jr.
-*   Date:       09/30/22
-*   Purpose:    This event should be generated when the user clicks the
-*               right mouse button. 
+/**********************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   09/30/22
+*   @file   OnContextMenu.h
 **************************************************************************/
 
 /*************************************************************************
@@ -30,13 +29,29 @@ namespace GU
 {
     namespace Evt
     {
+        /**********************************************************************//**
+        *   @class  OnContextMenu
+        *   @brief  This event should be generated when the user clicks the
+        *           right mouse button. 
+        **************************************************************************/
         class EVENT_EXPORT OnContextMenu: public GU::Evt::Event
         {
             public:
+                /******************************************************************//**
+                *   @brief  Constructor
+                *   @param  menuId is a unique identifier for the menu to be created. 
+                *   @param  line the line where the event originates from
+                *   @param  file the file where the event originates from
+                **********************************************************************/
                 OnContextMenu(const int &menuId, const int &line, const char* file = "");
+                
+
+                /******************************************************************//**
+                *   @brief  Destructor 
+                **********************************************************************/
                 virtual ~OnContextMenu();
             public:
-                const int m_menuId = -1;
+                const int m_menuId = -1;        /**< Unique identifier for the menu to be created. */
         };
     }
 }
--- libraries/src/GameUtilities/Event/Events/Mouse/OnContextMenu.cpp
+++ libraries/src/GameUtilities/Event/Events/Mouse/OnContextMenu.cpp
@@ -1,9 +1,26 @@
-/**************************************************************************
-*   Author:     Wayne J Larson Jr.
-*   Date:       09/30/22
-*   Purpose:    This event should be generated when the user clicks the
-*               right mouse button. 
+/**********************************************************************//**
+*   @author Wayne J Larson Jr.
+*   @date   09/30/22
+*   @file   OnContextMenu.cpp
 **************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "event_export.h"
 #include "GameUtilities/Event/Events/Mouse/OnContextMenu.h"
 #include "GameUtilities/Event/EventId.h"
@@ -12,6 +29,12 @@ namespace GU
 {
     namespace Evt
     {
+        /******************************************************************//**
+        *   @brief  Constructor
+        *   @param  menuId is a unique identifier for the menu to be created. 
+        *   @param  line the line where the event originates from
+        *   @param  file the file where the event originates from
+        **********************************************************************/
         OnContextMenu::OnContextMenu(const int &menuId, const int &line, const char* file):
         Event(static_cast<int>(GU::Evt::EventId::ON_CONTEXT_MENU), line, file),
         m_menuId(menuId)
@@ -20,6 +43,9 @@ namespace GU
         }
         
     
+        /******************************************************************//**
+        *   @brief  Destructor 
+        **********************************************************************/
         OnContextMenu::~OnContextMenu()
         {
 
