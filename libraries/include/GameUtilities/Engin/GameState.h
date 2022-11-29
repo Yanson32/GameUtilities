da1cf2b13d28b470b7fdda76cbc384c7c78e2da5
Yanson <wlarson30@gmail.com>
2022-11-29 17:11:26 -0500

    Updated the documentation for the GameState class

--- libraries/include/GameUtilities/Engin/GameState.h
+++ libraries/include/GameUtilities/Engin/GameState.h
@@ -1,8 +1,26 @@
 /*********************************************************************************//**
-*	\author	Wayne J Larson Jr.
-*	\date 12/10/17
+*	@author	Wayne J Larson Jr.
+*	@date   12/10/17
+*   @file   GameState.h
 *************************************************************************************/
 
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #ifndef GAMEUTILITIES_ENGIN_GAMESTATE_H
 #define GAMEUTILITIES_ENGIN_GAMESTATE_H
 #include "engin_export.h"
@@ -16,70 +34,72 @@ namespace GU
         class Engin;
 
         /*********************************************************************************//**
-        *	\brief	This is the base class used by all game states.
-        *	\class GameState
+        *	@brief	This is the base class used by all game states.
+        *	@class GameState
         *************************************************************************************/
         class ENGIN_EXPORT GameState
         {
             public:
+                /// Engin pointer typedef
                 typedef GU::Engin::Engin * const EnginPtr;
 
+
                 /*********************************************************************************//**
-                *   \brief	Default constructor
+                *   @brief	Default constructor
                 *************************************************************************************/
                 GameState();
 
 
                 /*********************************************************************************//**
-                *   \brief	Initialize the game state.
+                *   @brief	Initialize the game state.
                 *************************************************************************************/
                 virtual void init() = 0;
 
                 /*********************************************************************************//**
-                *   \brief	Clean any resource the state uses
+                *   @brief	Clean any resource the state uses
                 *************************************************************************************/
                 virtual void clean() = 0;
 
                 /*********************************************************************************//**
-                *   \brief	This method handles input such as user input and events
-                *	\param	engin is a reference to the game's Engin object.
-                *	\param	deltaTime is the length of time a single frame takes 
+                *   @brief	This method handles input such as user input and events
+                *	@param	engin is a reference to the game's Engin object.
+                *	@param	deltaTime is the length of time a single frame takes 
                 *************************************************************************************/
                 virtual void handleEvents(EnginPtr engin, const float &deltaTime) = 0;
 
 
                 /*********************************************************************************//**
-                *   \brief	This method handles input such as user input and events
-                *	\param	engin is a reference to the game's Engin object.
-                *	\param	deltaTime is the length of time a single frame takes 
+                *   @brief	This method handles input such as user input and events
+                *	@param	engin is a reference to the game's Engin object.
+                *	@param	deltaTime is the length of time a single frame takes 
                 *************************************************************************************/
                 virtual void update(EnginPtr engin, const float &deltaTime) = 0;
 
 
                 /*********************************************************************************//**
-                *   \brief	This method draws the current game state.
-                *	\param	engin is a reference to the game's Engin object.
-                *	\param	deltaTime is the length of time a single frame takes 
+                *   @brief	This method draws the current game state.
+                *	@param	engin is a reference to the game's Engin object.
+                *	@param	deltaTime is the length of time a single frame takes 
                 *************************************************************************************/
                 virtual void draw(EnginPtr engin, const float &deltaTime) = 0;
 
 
                 /*********************************************************************************//**
-                *   \brief	This method draws the current game state.
-                *	\param	pause when true the game will stop. When false the game will continue
+                *   @brief	This method draws the current game state.
+                *	@param	pause when true the game will stop. When false the game will continue
                 *************************************************************************************/
                 void pause(const bool pause = true);
 
 
                 /*********************************************************************************//**
-                *   \brief	This method returns a boolean true when the game is paused and false
+                *   @brief	This method returns a boolean true when the game is paused and false
                 *			otherwise
-                *	\return	boolean true when the game is paused and false otherwise
+                *	@return	boolean true when the game is paused and false otherwise
                 *************************************************************************************/
                 bool isPaused()const;
 
                 /*********************************************************************************//**
-                *   \brief	Destructor
+                *   @brief	Destructor
                 *************************************************************************************/
                 virtual ~GameState();
             private:
--- libraries/src/GameUtilities/Engin/GameState.cpp
+++ libraries/src/GameUtilities/Engin/GameState.cpp
@@ -1,3 +1,26 @@
+/*********************************************************************************//**
+*	@author	Wayne J Larson Jr.
+*	@date   12/10/17
+*   @file   GameState.cpp
+*************************************************************************************/
+
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* GameUtilities is a toolkit for making 2d video games.
+* Copyright (C) 2018 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 #include "GameUtilities/Engin/GameState.h"
 
 namespace GU
@@ -8,7 +31,7 @@ namespace GU
 
 
         /*********************************************************************************//**
-        *   \brief	Default constructor
+        *   @brief	Default constructor
         *************************************************************************************/
         GameState::GameState()
         {
@@ -17,8 +40,8 @@ namespace GU
 
 
         /*********************************************************************************//**
-        *   \brief	This method draws the current game state.
-        *	\param [in]	pause when true the game will stop. When false the game will continue
+        *   @brief This method draws the current game state.
+        *	@param pause when true the game will stop. When false the game will continue
         *************************************************************************************/
         void GameState::pause(const bool pause)
         {
@@ -27,9 +50,9 @@ namespace GU
 
 
         /*********************************************************************************//**
-        *   \brief	This method returns a boolean true when the game is paused and false
+        *   @brief	This method returns a boolean true when the game is paused and false
         *			otherwise
-        *	\return	boolean true when the game is paused and false otherwise
+        *	@return	boolean true when the game is paused and false otherwise
         *************************************************************************************/
         bool GameState::isPaused()const
         {
@@ -38,7 +61,7 @@ namespace GU
 
 
         /*********************************************************************************//**
-        *   \brief	Destructor
+        *   @brief	Destructor
         *************************************************************************************/
         GameState::~GameState()
         {
