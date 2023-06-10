7dcd0cffcb4d1a66db1fe54828bdf067cc086e47
Yanson32 <wlarson30@gmail.com>
2023-06-10 00:44:23 -0400

    Added help text to cmake script

---
+++ cmake/modules/help.cmake
@@ -0,0 +1,5 @@
+function(display_help_text)
+    message("Set GU_BUILD_TESTING to true to build tests. Tests depend on SFML")
+    message("Set GU_BUILD_DOC to true to build documentation")
+    message("Set GU_BUILD_EXAMPLES to true to build documentation")
+endfunction()
